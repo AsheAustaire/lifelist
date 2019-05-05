bd1d4f9fcb179180aa33ba99ef34946140c5857f
Ashe <asheaustaire@gmail.com>
2019-05-05 17:12:55 -0400

    fix_right_side

--- config.js
+++ config.js
@@ -41,4 +41,10 @@ const PRIORITIES = [
     "contract": "Do yoga in the morning and at night",
     "dateCreated": "May 5th, 2019"
   },
+  {
+    "title": "That",
+    "value": 0,
+    "contract": "Do yoga in the morning and at night",
+    "dateCreated": "May 5th, 2019"
+  },
 ]    
