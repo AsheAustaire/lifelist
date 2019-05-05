929b849b28c0ef665385cfad5d63420c12f3e3ac
Ashe <asheaustaire@gmail.com>
2019-05-05 17:05:54 -0400

    extract_css_data_logic

---
+++ config.js
@@ -0,0 +1,44 @@
+const PRIORITIES = [
+  {
+    "title": "Yoga",
+    "value": 0,
+    "contract": "Do yoga in the morning and at night",
+    "dateCreated": "May 5th, 2019"
+  },
+  {
+    "title": "Web Development",
+    "value": 0,
+    "contract": "Do yoga in the morning and at night",
+    "dateCreated": "May 5th, 2019"
+  },
+  {
+    "title": "Family",
+    "value": 0,
+    "contract": "Do yoga in the morning and at night",
+    "dateCreated": "May 5th, 2019"
+  },
+  {
+    "title": "Friends",
+    "value": 0,
+    "contract": "Do yoga in the morning and at night",
+    "dateCreated": "May 5th, 2019"
+  },
+  {
+    "title": "Work",
+    "value": 0,
+    "contract": "Do yoga in the morning and at night",
+    "dateCreated": "May 5th, 2019"
+  },
+  {
+    "title": "This",
+    "value": 0,
+    "contract": "Do yoga in the morning and at night",
+    "dateCreated": "May 5th, 2019"
+  },
+  {
+    "title": "That",
+    "value": 0,
+    "contract": "Do yoga in the morning and at night",
+    "dateCreated": "May 5th, 2019"
+  },
+]    
